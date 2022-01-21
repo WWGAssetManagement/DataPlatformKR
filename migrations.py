20f86e0e055106c1026ba084263eb99ddd35f049
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 12:52:02 +0900

    추가한 StockListModel Migration하는 코드 추가

--- migrations.py
+++ migrations.py
@@ -1,5 +1,6 @@
 from daeshin.models.minutepricemodel import MinutePriceModel
-from config.settings import DAESHIN_ENGINE
+from ebest.model.stocklistmodel import StockListModel
+from config.settings import DAESHIN_ENGINE, EBEST_ENGINE
 
 MinutePriceModel.__table__.create(bind=DAESHIN_ENGINE, checkfirst=True)
-
+StockListModel.__table__.create(bind=EBEST_ENGINE, checkfirst=True)
