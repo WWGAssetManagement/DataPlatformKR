b04136f58dc0c8c7554c96cfd123eecb774c5588
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 13:50:09 +0900

    종목정보 DB에 저장하는 기능 추가

--- ebest/tr/stocklist.py
+++ ebest/tr/stocklist.py
@@ -2,36 +2,49 @@
 StockList: 주식종목조회
 """
 from ebest.core.xa_query_events import XAQueryEvents
+from ebest.core.ebest_database import EbestDataBase
+from ebest.model.stocklistmodel import StockListModel
 from datetime import datetime
 
 
-class StockList(XAQueryEvents):
+class StockList(XAQueryEvents, EbestDataBase):
     tr_code = "t8430"
+    results = None
 
-    def __init__(self):
-        super().__init__(tr_code=self.tr_code)
+    def __init__(self, gubun):
+        """
+        :param gubun: 구분 0: 전체, 1: 코스피, 2: 코스닥
+        :return:
+        """
+        XAQueryEvents.__init__(self, tr_code=self.tr_code)
+        EbestDataBase.__init__(self, model=StockListModel)
+        self._set_field_data(self.inblock, "gubun", 0, gubun)
+        self._request(0)
+        self._receive()
+        self.__get_rows()
 
-    def set(self):
-        self.set_field_data(self.inblock, "gubun", 0, 0)
-        self.request()
-        self.receive()
+    def to_dicts(self) -> list:
+        return self.results
 
-    def get(self) -> list:
-        index_num = self.get_block_count(self.outblock)
-        return list(map(lambda x: self.__get_row(x), range(index_num)))
+    def to_sql(self):
+        self.save(self.results)
+
+    def __get_rows(self):
+        index_num = self._get_block_count(self.outblock)
+        self.results = list(map(lambda x: self.__get_row(x), range(index_num)))
 
     def __get_row(self, index) -> dict:
-        date = datetime.now()
-        hname = self.get_field_data(self.outblock, 'hname', index).strip()
-        shcode = self.get_field_data(self.outblock, 'shcode', index).strip()
-        expcode = self.get_field_data(self.outblock, "expcode", index).strip()
-        etfgubun = int(self.get_field_data(self.outblock, "etfgubun", index).strip())
-        uplmtprice = int(self.get_field_data(self.outblock, "uplmtprice", index).strip())
-        dnlmtprice = int(self.get_field_data(self.outblock, "dnlmtprice", index).strip())
-        jnilclose = int(self.get_field_data(self.outblock, "jnilclose", index).strip())
-        memedan = int(self.get_field_data(self.outblock, "memedan", index).strip())
-        recprice = int(self.get_field_data(self.outblock, "recprice", index).strip())
-        gubun = int(self.get_field_data(self.outblock, "gubun", index).strip())
+        date = datetime.now().strftime("%Y-%m-%d")
+        hname = self._get_field_data(self.outblock, 'hname', index).strip()
+        shcode = self._get_field_data(self.outblock, 'shcode', index).strip()
+        expcode = self._get_field_data(self.outblock, "expcode", index).strip()
+        etfgubun = int(self._get_field_data(self.outblock, "etfgubun", index).strip())
+        uplmtprice = int(self._get_field_data(self.outblock, "uplmtprice", index).strip())
+        dnlmtprice = int(self._get_field_data(self.outblock, "dnlmtprice", index).strip())
+        jnilclose = int(self._get_field_data(self.outblock, "jnilclose", index).strip())
+        memedan = int(self._get_field_data(self.outblock, "memedan", index).strip())
+        recprice = int(self._get_field_data(self.outblock, "recprice", index).strip())
+        gubun = int(self._get_field_data(self.outblock, "gubun", index).strip())
 
         return {
             'date': date,
