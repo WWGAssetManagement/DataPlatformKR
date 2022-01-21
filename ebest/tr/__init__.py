41b4a4c8c3da83b5efce119b124dcd5921136d0f
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 17:07:17 +0900

    업종 종목가저오는 객체 return해주는 함수 구현

--- ebest/tr/__init__.py
+++ ebest/tr/__init__.py
@@ -2,11 +2,16 @@ from ebest.tr.login import Login
 from ebest.tr.stocklist import StockList
 from ebest.tr.sectorcode import SectorCode
 from ebest.tr.themacode import ThemaCode
+from ebest.tr.sectorstock import SectorStock
 
 def get_login():
     return Login()
 
 def get_stock_list(gubun):
+    """
+    :param gubun:  구분 0: 전체, 1: 코스피, 2: 코스닥
+    :return:
+    """
     return StockList(gubun=gubun)
 
 def get_sector_code():
@@ -14,3 +19,10 @@ def get_sector_code():
 
 def get_thema_code():
     return ThemaCode()
+
+def get_sector_stock(upcode):
+    """
+    :param upcode: 업종코드
+    :return:
+    """
+    return SectorStock(upcode=upcode)
