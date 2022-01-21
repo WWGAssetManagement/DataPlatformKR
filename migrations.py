8c2e63b34b3d13db43e516fb7fbea98e3c8d4d87
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 15:33:22 +0900

    테마코드 모델 Migration에 추가

--- migrations.py
+++ migrations.py
@@ -1,8 +1,10 @@
 from daeshin.models.minutepricemodel import MinutePriceModel
 from ebest.model.stocklistmodel import StockListModel
 from ebest.model.sectorcodemodel import SectorCodeModel
+from ebest.model.themacodemodel import ThemaCodeModel
 from config.settings import DAESHIN_ENGINE, EBEST_ENGINE
 
 MinutePriceModel.__table__.create(bind=DAESHIN_ENGINE, checkfirst=True)
 StockListModel.__table__.create(bind=EBEST_ENGINE, checkfirst=True)
 SectorCodeModel.__table__.create(bind=EBEST_ENGINE, checkfirst=True)
+ThemaCodeModel.__table__.create(bind=EBEST_ENGINE, checkfirst=True)
