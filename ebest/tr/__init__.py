50614b2458708ba9fc3e2c984112db3b756e3da2
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 15:35:52 +0900

    섹터코드와 테마코드 객체 return해주는 함수 추가

--- ebest/tr/__init__.py
+++ ebest/tr/__init__.py
@@ -1,11 +1,16 @@
 from ebest.tr.login import Login
 from ebest.tr.stocklist import StockList
-
+from ebest.tr.sectorcode import SectorCode
+from ebest.tr.themacode import ThemaCode
 
 def get_login():
     return Login()
 
-
 def get_stock_list(gubun):
     return StockList(gubun=gubun)
 
+def get_sector_code():
+    return SectorCode()
+
+def get_thema_code():
+    return ThemaCode()
