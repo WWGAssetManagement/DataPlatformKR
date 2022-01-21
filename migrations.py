bc21f22b78e5fca107aff1357c0f0e59148eb740
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 13:51:18 +0900

    SectorCodeModel Migration하는 코드 추가

--- migrations.py
+++ migrations.py
@@ -1,6 +1,8 @@
 from daeshin.models.minutepricemodel import MinutePriceModel
 from ebest.model.stocklistmodel import StockListModel
+from ebest.model.sectorcodemodel import SectorCodeModel
 from config.settings import DAESHIN_ENGINE, EBEST_ENGINE
 
 MinutePriceModel.__table__.create(bind=DAESHIN_ENGINE, checkfirst=True)
 StockListModel.__table__.create(bind=EBEST_ENGINE, checkfirst=True)
+SectorCodeModel.__table__.create(bind=EBEST_ENGINE, checkfirst=True)
