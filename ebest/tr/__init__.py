43b21b9c97c561a8d06dcf4dd884dca40a19f16a
YoungMinChoi <dudals3844@gmail.com>
2022-01-21 13:53:01 +0900

    종목정보 객체 return해주는 함수 구현

--- ebest/tr/__init__.py
+++ ebest/tr/__init__.py
@@ -1,5 +1,11 @@
 from ebest.tr.login import Login
+from ebest.tr.stocklist import StockList
 
 
 def get_login():
     return Login()
+
+
+def get_stock_list(gubun):
+    return StockList(gubun=gubun)
+
