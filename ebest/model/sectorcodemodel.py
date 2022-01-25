948e7b256be393f5444e68febd6b39f8673e930a
YoungMinChoi <dudals3844@gmail.com>
2022-01-25 09:23:03 +0900

    models.py로 이동

--- ebest/model/sectorcodemodel.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, VARCHAR
-from config.settings import BASE
-
-
-class SectorCodeModel(BASE):
-    __tablename__ = "tb_sector_code"
-    upcode = Column(VARCHAR(5), primary_key=True, nullable=False)
-    hname = Column(VARCHAR(30))
